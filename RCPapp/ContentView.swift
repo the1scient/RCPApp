029adb76236a1b9bee7b94cc5da70f81b6f88dec
notfarchi <jv.farchi@hotmail.com>
2023-09-22 08:42:39 -0300

    subindo teste

---
+++ RCPapp/ContentView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  RCPapp
+//
+//  Created by Student16 on 19/09/23.
+//
+
+import SwiftUI
+
+struct Music{
+    var name: String
+    var artist: String
+    var image: String
+}
+
+struct ContentView: View {
+    
+    
+    
+    var musicList = [
+        Music(name: "Song 1", artist: "Artist 1", image: "https://i.scdn.co/image/ab67616d0000b273fc915b69600dce2991a61f13"),
+        Music(name: "Song 2", artist: "Artist 2", image: "image2"),
+        // Adicione mais músicas conforme necessário
+    ]
+    
+    var body: some View {
+        
+        
+        TabView {
+            
+            
+            VStack{
+                VStack{
+                    Text("RCPApp").font(.title)
+                    Image(systemName: "heart.fill").foregroundColor(.red)
+                        .font(.system(size: 30))
+                    
+                    Circle().padding(.top).frame(width: 200.0, height: 200.0)
+                    
+                }
+                Spacer()
+                
+                VStack{
+                    ForEach(musicList, id: \.name){ musica in
+                        VStack{
+                            HStack{
+                                Rectangle().frame(width: 100.0, height: 100.0)
+                                Spacer()
+                                
+                            }.padding()
+                        }
+                        //Text(musica.image)
+                        //                  AsyncImage(url: URL(string: musica.image))
+                        
+                    }
+                    
+                    
+                }.padding(.trailing)
+                Spacer()
+                
+            }
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            Text("infos")
+                .font(.title)
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("infos")
+                }
+            Text("Mapa")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "map.circle")
+                    Text("Mapa")
+                }
+            Text("perfil")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("perfil")
+                }
+        }
+        
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ RCPapp/RCPappApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCPappApp.swift
+//  RCPapp
+//
+//  Created by Student16 on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RCPappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
