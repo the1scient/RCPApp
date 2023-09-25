3559f6d7847362184c7756412af190c3bc017671
Guilherme Fabbri <guilherme.ffabbri@senacsp.edu.br>
2023-09-25 09:16:27 -0300

    creating a playing state and on tap the circle with music updates

--- RCPapp/ContentView.swift
+++ RCPapp/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Music{
+struct Music {
     var name: String
     var artist: String
     var image: String
@@ -15,14 +15,19 @@ struct Music{
 
 struct ContentView: View {
     
-    
-    
+
     var musicList = [
         Music(name: "Bones", artist: "Imagine Dragons", image: "https://i.scdn.co/image/ab67616d0000b273fc915b69600dce2991a61f13"),
         Music(name: "do wanna taste it", artist: "Wig Wam", image: "https://static.qobuz.com/images/covers/kb/kc/t8o6zveb8kckb_600.jpg"),
         Music(name: "annihilation", artist: " Lil Wayne, Metro Boomin, Offset, Swae Lee 2", image: "https://i.scdn.co/image/ab67616d0000b2736ed9aef791159496b286179f"),
         // Adicione mais músicas conforme necessário
     ]
+    
+    @State var isPlaying : Bool = false;
+    @State private var isRotating = 0.0
+
+    
+    @State var currentPlaying : Music =  Music(name: "Bones", artist: "Imagine Dragons", image: "https://i.scdn.co/image/ab67616d0000b273fc915b69600dce2991a61f13")
     
     var body: some View {
         
@@ -36,8 +41,25 @@ struct ContentView: View {
                     Image(systemName: "heart.fill").foregroundColor(.red)
                         .font(.system(size: 30))
                     
-                    Circle().padding(.top).frame(width: 200.0, height: 200.0)
+                  
+                    AsyncImage(url: URL(string: currentPlaying.image)){ image in
+                        image
+                           
+                            .resizable()
+                            .clipShape(Circle())
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 200, height: 200)
+                        
+                    }
+                placeholder: {
                     
+                }
+                    
+                     
+                    
+                
+                    
+                     
                 }
                 Spacer()
                     .frame(height: 50)
@@ -59,6 +81,12 @@ struct ContentView: View {
                                     }
                                     .frame(width: 120, height: 120)
                                     .clipShape(Circle())
+                                    
+                                    .onTapGesture {
+                                        currentPlaying = musica
+                                    }
+                                 
+                                  
                                     
                                     VStack (alignment: .leading){
                                         Text(musica.name).font(.title2).fontWeight(.semibold)
